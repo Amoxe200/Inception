6ffb4377d952653cc1cb66dea34331fa13221e8a
Aiman Aqari <aaqari@e1r12p4.1337.ma>
2022-09-02 07:09:51 +0100

    s

--- srcs/requirements/wordpress/wp-sh.sh
+++ srcs/requirements/wordpress/wp-sh.sh
@@ -5,7 +5,7 @@ mkdir -p /var/www/html/wp
 
 wp --path=/var/www/html/wp core download --allow-root
 wp --path=/var/www/html/wp config create --dbname=wordpress --dbuser=mariadb_user --dbpass=secret --dbhost=mariadb --locale=en_US --allow-root
-wp --path=/var/www/html/wp core install --url=https://localhost/wp/ --title=Inception --admin_user=wpadmin --admin_password=secret --admin_email=admin@email.com --skip-email  --allow-root
+wp --path=/var/www/html/wp core install --url=https://aaqari.42.fr --title=Inception --admin_user=wpadmin --admin_password=secret --admin_email=admin@email.com --skip-email  --allow-root
 
 fi
 
