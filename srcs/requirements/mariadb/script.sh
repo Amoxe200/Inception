0a7ec4a1732903dabd736727b277658274c84909
Aiman Aqari <aaqari@e1r12p4.1337.ma>
2022-09-02 06:38:09 +0100

    lat

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -11,8 +11,8 @@ n
 n
 y
 EOF
-mysql -u root -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY secret;"
-mysql -u root -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY secret;"
+mysql -u root -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY 'secret';"
+mysql -u root -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY 'secret';"
 mysql -u root -e "GRANT ALL ON *.* TO '${DBUSER}'@'localhost';"
 mysql -u root -e "GRANT ALL ON *.* TO '${DBUSER}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
